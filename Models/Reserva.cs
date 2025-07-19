8b13d12f58c2c79120fb8848621e24e4316ead90
Thiago Marques <thiago77mar@gmail.com>
2025-07-19 13:03:57 -0300

    Primeiro commit

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,42 +16,38 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite != null && hospedes.Count <= Suite.Capacidade)//Se a Suite existir e a quantidade de hospedes for menor que a capacidade da Suite
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException("O número de hóspedes é maior que a capacidade da suíte");
             }
         }
 
         public void CadastrarSuite(Suite suite)
         {
-            Suite = suite;
+            Suite = suite;//cadastra a suite.
         }
 
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes?.Count ?? 0;//se Hospedes não for nulo, retorna a quantidade de hóspedes, senão retorna 0.
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9M;//desconto de 10%.
             }
 
             return valor;
